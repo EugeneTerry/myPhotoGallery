772776e4165d86c00de5e35824fb0fb66828e28a
Eugene Terry <eugene.terry73@gmail.com>
2022-02-20 12:09:11 -0600

    added new

--- scripts/database.js
+++ scripts/database.js
@@ -1,5 +1,5 @@
 export const database = {
   image: "https://randomwordgenerator.com/img/picture-generator/53e1d2474c53a814f1dc8460962e33791c3ad6e04e50744074267bd29e45c4_640.jpg",
-  discription: "picture from database"
+  discription: "picture from the database"
 
 }
